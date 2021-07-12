e58c563b32b68f92b2a7832075c10cab0c425524
cssaranya <57591374+cssaranya@users.noreply.github.com>
2021-07-12 21:23:00 +0200

    Add files via upload

--- managecustaccount/src/main/java/com/capgemini/managecustaccount/controller/CustomerAccount.java
+++ managecustaccount/src/main/java/com/capgemini/managecustaccount/controller/CustomerAccount.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.managecustaccount.DTO.AccountDTO;
@@ -38,7 +39,7 @@ public class CustomerAccount {
 	private TransactionService txnService;
 	
 	@PostMapping(value = "currAccount")
-	public ResponseEntity<AccountDTO> createCurrAccount(Long customerId, double initialCredit) {
+	public ResponseEntity<AccountDTO> createCurrAccount(@Valid @RequestParam Long customerId, @Valid @RequestParam double initialCredit) {
 		logger.info("Creating new current account for the customer "+customerId);
 		
 		CustomerDTO customer = custService.getCustomer(customerId);
@@ -51,7 +52,7 @@ public class CustomerAccount {
 	}
 	
 	@GetMapping(value = "AccountTxn")
-	public ResponseEntity<Object> getAccountTransaction(Long customerId, String accountType) {
+	public ResponseEntity<Object> getAccountTransaction(@Valid @RequestParam Long customerId, @Valid @RequestParam String accountType) {
 		logger.info("Fetching transaction information of "+accountType+" account for "+customerId);
 		
 		CustomerDTO customer = custService.getCustomer(customerId);
--- managecustaccount/src/main/java/com/capgemini/managecustaccount/entity/Account.java
+++ managecustaccount/src/main/java/com/capgemini/managecustaccount/entity/Account.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -21,6 +23,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class Account {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long accountNumber;
 	
 	@NotNull
